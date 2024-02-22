4e014dd287ecb7614440a78cf163974c351cb480
shobhit <shobhit32t@gmail.com>
2024-02-22 20:35:25 +0530

    Error handling

--- src/read_resource.c
+++ src/read_resource.c
@@ -7,12 +7,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdlib.h>
 
-// assumes a valid filepath is given
-// the content of the file is written to char *content
-// free() the pointer returned by the function
+// returns pointer to a dynamically allocated char memory block
+// containing the contents of the file to be read
+// free() the pointer returned by the function after use
 char* readFile_dyn(char *filepath) {
     FILE *fp = fopen(filepath, "r");
+    if(fp == NULL) {
+        perror("Can't open file for reading");
+        exit(EXIT_FAILURE);
+    }
     char line[100];
     long l;
     fseek(fp, 0L, SEEK_END);
--- src/read_resource.h
+++ src/read_resource.h
@@ -5,9 +5,9 @@
 #ifndef DASHSERVER_READ_RESOURCE_H
 #define DASHSERVER_READ_RESOURCE_H
 
-// assumes a valid filepath is given
-// returns a pointer to the content of the file
-// free() the pointer returned by the function
+// returns pointer to a dynamically allocated char memory block
+// containing the contents of the file to be read
+// free() the pointer returned by the function after use
 char* readFile_dyn(char *filepath);
 
 #endif //DASHSERVER_READ_RESOURCE_H
