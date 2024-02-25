0a9ba8d076da2378d37b6b627d2a50344c0c3ce2
shobhit <shobhit32t@gmail.com>
2024-02-26 03:17:02 +0530

    Improved naming of variables

--- src/read_resource.c
+++ src/read_resource.c
@@ -11,23 +11,27 @@
 
 // returns pointer to a dynamically allocated char memory block
 // containing the contents of the file to be read
-// free() the pointer returned by the function after use
+// need to free() the pointer returned by the function after use
 char* readFile_dyn(char *filepath) {
     FILE *fp = fopen(filepath, "r");
     if(fp == NULL) {
         perror("Can't open file for reading");
         exit(EXIT_FAILURE);
     }
-    char line[100];
-    long l;
+    char curr_line[MAX_LEN_FILELINE];
+
+    // calculate total number of characters in the file
+    long file_total_char;
     fseek(fp, 0L, SEEK_END);
-    l = ftell(fp);
-    rewind(fp);
-    char *content = (char*) malloc(l);
-    while(fgets(line, 100, fp)) {
-        strcat(content, line);
+    file_total_char = ftell(fp);
+
+    rewind(fp); // sets the file position indicator back to start position
+
+    char *content = (char*) malloc(file_total_char + 1);
+    while(fgets(curr_line, MAX_LEN_FILELINE, fp)) {
+        strcat(content, curr_line);
     }
-    content[l-1] = '\0';
+    content[file_total_char - 1] = '\0';
     fclose(fp);
     return content;
 }
--- src/read_resource.h
+++ src/read_resource.h
@@ -5,9 +5,11 @@
 #ifndef DASHSERVER_READ_RESOURCE_H
 #define DASHSERVER_READ_RESOURCE_H
 
+#define MAX_LEN_FILELINE 1000 // max characters assumed in a line of the file read
+
 // returns pointer to a dynamically allocated char memory block
 // containing the contents of the file to be read
-// free() the pointer returned by the function after use
+// need to free() the pointer returned by the function after use
 char* readFile_dyn(char *filepath);
 
 #endif //DASHSERVER_READ_RESOURCE_H
